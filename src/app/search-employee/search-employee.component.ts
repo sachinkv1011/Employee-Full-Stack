a599a12e50bd683f71c69aa579cd3da040dcde2e
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-08 03:42:34 +0530

    integration

--- src/app/search-employee/search-employee.component.ts
+++ src/app/search-employee/search-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search-employee',
@@ -7,12 +8,26 @@ import { Component } from '@angular/core';
 })
 export class SearchEmployeeComponent {
 
+  constructor(private api:ApiService){}
   empCode=""
-
+  searchData:any=[]
   readValue=()=>{
     let data:any={
       "empCode":this.empCode
     }
     console.log(data)
+    this.api.searchData(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.length==0) {
+          alert("Invalid employee code")
+          
+        } else {
+          this.searchData=response
+          
+        }
+
+      }
+    )
   }
 }
